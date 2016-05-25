2575084e8b09300dd0594a6bd650f69091f858ea
tony-luisi <t.luisi@gmail.com>
2016-05-25 15:00:27 +1200

    adding typing css

--- components/app/main.js
+++ components/app/main.js
@@ -5,6 +5,12 @@ export default class Main extends Component{
     return (
       <div>
         <h1>Main</h1>
+        <div className='type'>
+          <h3>Hello</h3>
+          <h4>And welcome to my site</h4>
+        </div>
+
+
       </div>
     )
   }
