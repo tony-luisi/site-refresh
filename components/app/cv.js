af989699698ed202859fdb4289f028dde337b6df
tony-luisi <t.luisi@gmail.com>
2016-05-29 14:04:39 +1200

    updated cv fade in animation

--- components/app/cv.js
+++ components/app/cv.js
@@ -8,7 +8,7 @@ export default class CV extends Component{
   render(){
     return (
       <div>
-        <h1>
+        <h1 className="animated fadeIn">
           <a href="https://drive.google.com/open?id=0B1IPq-XwsCRuRjFnWmx3cms5NUk" target="_blank">Full CV Click Here</a>
         </h1>
       </div>
