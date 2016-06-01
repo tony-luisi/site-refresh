2424b3464793b6cbb1d4fc772ab73fd5ee039307
tony-luisi <t.luisi@gmail.com>
2016-06-01 14:11:40 +1200

    added linter

--- src/js/data/projects.js
+++ src/js/data/projects.js
@@ -1,56 +1,56 @@
 export const projects = [
   {
-    title: "on fleek",
-    projectType: "group",
-    description: "an app that allows users to feel better about themselves",
-    technologies: ["react", "redux", "express"],
-    github: "",
-    deployment: "",
-    imageLink: ""
+    title: 'on fleek',
+    projectType: 'group',
+    description: 'an app that allows users to feel better about themselves',
+    technologies: ['react', 'redux', 'express'],
+    github: '',
+    deployment: '',
+    imageLink: ''
   },
   {
-    title: "pokemon trading card game",
-    projectType: "group",
-    description: "an app that allows users to feel better about themselves",
-    technologies: ["react", "redux", "express"],
-    github: "",
-    deployment: "",
-    imageLink: ""
+    title: 'pokemon trading card game',
+    projectType: 'group',
+    description: 'an app that allows users to feel better about themselves',
+    technologies: ['react', 'redux', 'express'],
+    github: '',
+    deployment: '',
+    imageLink: ''
   },
   {
-    title: "ki mai",
-    projectType: "personal",
-    description: "an app that allows users to feel better about themselves",
-    technologies: ["react", "redux", "express"],
-    github: "",
-    deployment: "",
-    imageLink: ""
+    title: 'ki mai',
+    projectType: 'personal',
+    description: 'an app that allows users to feel better about themselves',
+    technologies: ['react', 'redux', 'express'],
+    github: '',
+    deployment: '',
+    imageLink: ''
   },
   {
-    title: "who hi",
-    projectType: "group",
-    description: "an app that allows users to feel better about themselves",
-    technologies: ["react", "redux", "express"],
-    github: "",
-    deployment: "",
-    imageLink: ""
+    title: 'who hi',
+    projectType: 'group',
+    description: 'an app that allows users to feel better about themselves',
+    technologies: ['react', 'redux', 'express'],
+    github: '',
+    deployment: '',
+    imageLink: ''
   },
   {
-    title: "conways game of life",
-    projectType: "group",
-    description: "an app that allows users to feel better about themselves",
-    technologies: ["react", "redux", "express"],
-    github: "",
-    deployment: "",
-    imageLink: ""
+    title: 'conways game of life',
+    projectType: 'group',
+    description: 'an app that allows users to feel better about themselves',
+    technologies: ['react', 'redux', 'express'],
+    github: '',
+    deployment: '',
+    imageLink: ''
   },
   {
-    title: "timeberlake",
-    projectType: "group",
-    description: "an app that allows users to feel better about themselves",
-    technologies: ["react", "redux", "express"],
-    github: "",
-    deployment: "",
-    imageLink: ""
+    title: 'timeberlake',
+    projectType: 'group',
+    description: 'an app that allows users to feel better about themselves',
+    technologies: ['react', 'redux', 'express'],
+    github: '',
+    deployment: '',
+    imageLink: ''
   }
 ]
