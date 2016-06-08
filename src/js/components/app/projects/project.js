bf5e47478554f0b6e857d00753647eb0cdbf175e
tony-luisi <t.luisi@gmail.com>
2016-06-08 14:17:29 +1200

    fixed columns in projects

--- src/js/components/app/projects/project.js
+++ src/js/components/app/projects/project.js
@@ -10,26 +10,28 @@ export default class Project extends Component{
     return(
       <div>
         <Col md={6}>
-          <Image src={this.props.imageLink} responsive></Image>
-        </Col>
-        <Col md={6}>
-          <h3>{this.props.name}</h3>
-          <h4>Description:</h4>
-          <p>{this.props.description}</p>
-          <h4>Project Type:</h4>
-          <p>{this.props.projectType}</p>
-          <h4>Technologies:</h4>
-          <ul>{this.props.technologies.map(function(technology){
-            return (
-              <li>
-                {technology}
-              </li>
-            )
-          })}</ul>
-          <h4>Github:</h4>
-          <p>{this.props.github}</p>
-          <h4>Deployment:</h4>
-          <p>{this.props.deployment}</p>
+          <Col md={6}>
+            <Image src={this.props.imageLink} responsive></Image>
+          </Col>
+          <Col md={6}>
+            <h3>{this.props.name}</h3>
+            <h4>Description:</h4>
+            <p>{this.props.description}</p>
+            <h4>Project Type:</h4>
+            <p>{this.props.projectType}</p>
+            <h4>Technologies:</h4>
+            <ul>{this.props.technologies.map(function(technology){
+              return (
+                <li>
+                  {technology}
+                </li>
+              )
+            })}</ul>
+            <h4>Github:</h4>
+            <p>{this.props.github}</p>
+            <h4>Deployment:</h4>
+            <p>{this.props.deployment}</p>
+          </Col>
         </Col>
       </div>
     )
